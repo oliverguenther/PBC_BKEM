34ba0a14193d53213fb767d1b3a8e0f10ce25237
Oliver Guenther <mail@oliverguenther.de>
2012-04-05 11:33:22 +0200

    small naming changes

--- pbc_bes.c
+++ pbc_bes.c
@@ -27,22 +27,22 @@
 #include <stdio.h>
 #include <math.h>
 
-#include "PBC_bes.h"
+#include "pbc_bes.h"
 
 void setup_global_system(bes_global_params_t *gps, const char *pstr, int N) {
 	// init global parameters
 	bes_global_params_t params;
 	params = pbc_malloc(sizeof(struct bes_global_params_s));
-
+    
 	params->N = N;
-
+    
 	// Compute A, B system params
 	params->B = (int) sqrt(N);
 	params->A = (int) N/params->B;
-
+    
 	// Init pairing
 	pairing_init_set_str(params->pairing, pstr);
-
+    
 	*gps = params;
 }
 
@@ -52,19 +52,19 @@ void setup(bes_system_t *sys, bes_global_params_t gps) {
 	bes_system_t gbs;
 	gbs = pbc_malloc(sizeof(struct bes_system_s));
     gbs->PK = pbc_malloc(sizeof(struct pubkey_s));
-
+    
 	// Choose random generator
 	element_init_G1(gbs->PK->g, gps->pairing);
 	element_random(gbs->PK->g);
-
+    
 	// random alpha Zn
 	element_t alpha;
 	element_init_Zr(alpha, gps->pairing);
 	element_random(alpha);
-
+    
 	// Compute g_i's
 	gbs->PK->g_i = pbc_malloc(2 * gps->B * sizeof(element_t));
-
+    
 	// Set the first element to g^(alpha^1)
 	element_init_G1(gbs->PK->g_i[0], gps->pairing);
 	element_pow_zn(gbs->PK->g_i[0],gbs->PK->g, alpha);
@@ -75,7 +75,7 @@ void setup(bes_system_t *sys, bes_global_params_t gps) {
 		element_init_G1(gbs->PK->g_i[i], gps->pairing);
 		element_pow_zn(gbs->PK->g_i[i], gbs->PK->g_i[i-1], alpha);
 	}
-
+    
 	// Choose random gamma_i and set v_i
 	element_t *gamma_i;
     gamma_i = pbc_malloc(gps->A * sizeof(struct element_s));    
@@ -86,17 +86,17 @@ void setup(bes_system_t *sys, bes_global_params_t gps) {
         element_random(gamma_i[i]);
 		element_pow_zn(gbs->PK->v_i[i], gbs->PK->g, gamma_i[i]);
 	}
-
+    
 	// Compute private keys d_i
     gbs->d_i = pbc_malloc(gps->N * sizeof(struct element_s));
 	for (i = 0; i < gps->N; i++) {
 		int a = (int) ceil(i/gps->B);
 		int b = (int) i % gps->B;
-
+        
 		element_init_G1(gbs->d_i[i], gps->pairing);
 		element_pow_zn(gbs->d_i[i], gbs->PK->g_i[b], gamma_i[a]);
 	}
-
+    
 	*sys = gbs;	
 	element_clear(alpha);
     for (i = 0; i < gps->A; ++i) {
@@ -105,34 +105,34 @@ void setup(bes_system_t *sys, bes_global_params_t gps) {
 }
 
 void get_encryption_key(keypair_t *key, int *S, int num_recip, bes_system_t sys, bes_global_params_t gps) {
-
+    
 	keypair_t kp;
 	kp = pbc_malloc(sizeof(struct keypair_s));
-
+    
 	// Init header, g^t , A instances
 	kp->HDR = pbc_malloc((gps->A + 1) * sizeof(element_t));
-
+    
 	// Get random t
 	element_t t;
     element_init_Zr(t, gps->pairing);
 	element_random(t);
-
+    
 	// Compute K = e(g_B, g_0)^t
 	element_init_GT(kp->K, gps->pairing);
 	pairing_apply(kp->K, sys->PK->g_i[gps->B-1], sys->PK->g_i[0], gps->pairing);
     element_pow_zn(kp->K, kp->K, t);
-
+    
 	// Set first header element to g^t
     element_init_G1(kp->HDR[0], gps->pairing);
 	element_pow_zn(kp->HDR[0], sys->PK->g, t);
-
+    
 	// Init HDR 1-A with v_i
 	int i;
 	for (i = 1; i <= gps->A; ++i) {
         element_init_G1(kp->HDR[i], gps->pairing);
 		element_set(kp->HDR[i], sys->PK->v_i[i-1]);
 	}
-
+    
 	// Define Subsets
 	int line, pos;
 	for (i = 0; i < num_recip; ++i) {
@@ -140,7 +140,7 @@ void get_encryption_key(keypair_t *key, int *S, int num_recip, bes_system_t sys,
 			printf("Element %d of receivers out of range\n", i);
 			return;
 		}
-
+        
 		// Get relative position of member S[i] within its subset
 		// Determine position in HDR (+1 offset from first element)
 		line = ((int) (S[i] / gps->A)) + 1;
@@ -148,65 +148,65 @@ void get_encryption_key(keypair_t *key, int *S, int num_recip, bes_system_t sys,
 		pos = (S[i] % gps->B) + 1; 	
 		element_mul(kp->HDR[line], kp->HDR[line], sys->PK->g_i[gps->B - pos]);
 	}
-
+    
 	// Pow each subinstance with t
 	for (i = 1; i <= gps->A; ++i) {
 		element_pow_zn(kp->HDR[i], kp->HDR[i], t);
 	}
-
+    
 	*key = kp;
 	element_clear(t);
-
+    
 }
 
 void get_decryption_key(element_t K, bes_global_params_t gps, int *S, int num_recip, int index, 
-             element_t d_i, element_t *HDR, pubkey_t PK) { 
-
+                        element_t d_i, element_t *HDR, pubkey_t PK) { 
+    
 	// a is equal to instance
 	int a = ((int) (index / gps->A)) + 1;
-
+    
 	// b is relative position in subset
 	int b = index % gps->B;
-
+    
     element_t nom, den, temp;
 	element_init_GT(nom, gps->pairing);
     // Set nominator to e(g_b, HDR_a)
 	pairing_apply(nom, PK->g_i[b], HDR[a], gps->pairing);
-
+    
     element_init_same_as(temp, d_i);
 	element_set(temp, d_i);
-
+    
 	int i, line, pos, pkpos;
 	for (i = 0; i < num_recip; ++i) {
 		if (S[i] < 0 || S[i] > gps->N) {
 			printf("Element %d of receivers out of range\n", i);
 			return;
 		}
-
+        
 		// Get relative position of member S[i] within its subset
 		// Determine position in HDR (+1 offset from first element)
 		line = ((int) (S[i] / gps->A)) + 1;
 		// Determine position
 		pos = (int) S[i] % gps->B;	
-
+        
         if (PBCBES_DEBUG)
             printf("index=%d, S[%d]=%d, subset nr=%d, relative pos=%d (a=%d, b=%d)\n",index, i, S[i], line, pos, a, b);
-
+        
 		if (line == a && pos != b) {
 			pkpos = (gps->B + 1) - pos+b;
 			element_mul(temp, temp, PK->g_i[pkpos-1]); 
 		}
 	}
-
+    
 	element_init_GT(den, gps->pairing);
 	pairing_apply(den, temp, HDR[0], gps->pairing);
-
+    
 	element_init_GT(K, gps->pairing);
 	element_div(K, nom, den);
-
+    
 	element_clear(temp);
 	element_clear(nom);
 	element_clear(den);
-
+    
 }
 
--- testscheme.c
+++ testscheme.c
@@ -1,4 +1,4 @@
-#include "bes_bgw.h"
+#include "pbc_bes.h"
 
 
 int main(void) {
@@ -10,7 +10,7 @@ int main(void) {
     printf("\nSystem setup Key\n\n");
 
 	bes_global_params_t gps;
-	setup_global_system(&gps, (const char*) buf, 16);
+	setup_global_system(&gps, (const char*) buf, 128);
 
 	printf("Global System parameters: N = %d, A = %d, B = %d\n\n", gps->N, gps->A, gps->B);
 
@@ -20,9 +20,9 @@ int main(void) {
     printf("\nTesting system\n\n");
     
     
-    int c,k;
-    for (c = 2; c <= 16; ++c) {
-//        if (c == 3) return;
+    unsigned int c,k,j;
+    for (c = 2; c <= gps->N; c+=2) {
+        //        if (c == 3) return;
         int S[c];
         printf("Testing with S = [ ");
         for (k = 0; k < c; ++k) {
@@ -30,43 +30,24 @@ int main(void) {
             printf("%d ", k);
         }
         printf("]\n");
+        keypair_t keypair;
+        get_encryption_key(&keypair, S, c, sys, gps);
+        element_t K;
+
         
-        int j;
-        for (j = 0; j < 16; ++j) {
-            keypair_t keypair;
-            encrypt(&keypair, S, c, sys, gps);
-            element_t K;
-            decrypt(K, gps, S, c, j, sys->d_i[j], keypair->HDR, sys->PK);
+        for (j = 0; j < gps->N; ++j) {
+            get_decryption_key(K, gps, S, c, j, sys->d_i[j], keypair->HDR, sys->PK);
             if (!element_cmp(keypair->K, K)) {
                 if (j >= c)
-                    printf("ERROR: Decryption Key for [User %d] matches, but should NOT\n", j);
-                else
-                    printf("SUCCESS: Decryption Key for [User %d] matches\n", j);            
+                    printf("ERROR: Decryption Key for [User %d] matches, but should NOT\n", j);       
             } else {
                 if (j < c)
                     printf("ERROR: Decryption Key for [User %d] does not match!\n", j);
             }
             element_clear(K);
-            free(keypair);
         }
+        free(keypair);
+        
     }
-    
-//    // Set first subset as receivers
-//    int S[] = {0,1};
-//    int c = 2;
-//    
-//    keypair_t keypair;
-//    encrypt(&keypair, S, c, sys, gps);
-//        
-//    
-//    printf("\nTesting Decryption\n\n");
-//    element_t K;
-//    decrypt(K, gps, S, c, 0, sys->d_i[0], keypair->HDR, sys->PK);
-//    
-//    if (!element_cmp(keypair->K, K)) {
-//        printf("Decryption successful. Key K matches\n");
-//    } else {
-//        printf("Key K does not match!\n");
-//    }
     
 }
